3a2a55ca5dd99ddffed6d91f3a113a3a68e05ba2
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2017-11-19 13:22:15 +0100

    add meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,8 @@ module.exports = {
     title: 'GEAR – genome analysis server',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Genomics web applications hosted at the EMBL'}
     ]
   },
   css: [
