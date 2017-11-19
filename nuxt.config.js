8e0b328c6e03a57de93ae27376aa5849c9dfa865
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2017-11-19 09:44:51 +0100

    use nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  head: {
+    title: 'GEAR – genome analysis server',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  },
+  css: [
+    '~/assets/css/style.css'
+  ]
+}
