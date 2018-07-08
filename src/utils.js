240e6767d5abf5ffd40dece350324ad5bbcc63b9
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-07-08 17:00:48 +0200

    prettier

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,11 @@
-function toSrgb (c) {
+function toSrgb(c) {
   c /= 255
   return c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4)
 }
 
-export function relativeLuminance (r, g, b) {
-  const sR = toSrgb(r), sG = toSrgb(g), sB = toSrgb(b)
+export function relativeLuminance(r, g, b) {
+  const sR = toSrgb(r),
+    sG = toSrgb(g),
+    sB = toSrgb(b)
   return 0.2126 * sR + 0.7152 * sG + 0.0722 * sB
 }
