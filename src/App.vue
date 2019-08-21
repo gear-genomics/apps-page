d49b18c2f4e0a1317e292a8f595f5d71a29d7333
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-08-21 07:34:37 +0200

    vuetify mvp

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <img src="@/assets/gear-g.png" height="75%" class="mx-4" />
+      <v-toolbar-title class="headline text-uppercase d-none d-sm-flex">
+        <span>GEAR</span>
+        <span class="font-weight-light">-GENOMICS</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" href="https://github.com/gear-genomics">
+            <v-icon left>fab fa-github</v-icon>
+            <span class="d-none d-md-flex">GitHub</span>
+          </v-btn>
+        </template>
+        <span>GitHub</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" href="https://gear-genomics.com/contact/">
+            <v-icon left>far fa-envelope</v-icon>
+            <span class="d-none d-md-flex">Contact</span>
+          </v-btn>
+        </template>
+        <span>Contact Us</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" href="https://gear-genomics.com/terms/">
+            <v-icon left>fas fa-balance-scale</v-icon>
+            <span class="d-none d-md-flex">TOS</span>
+          </v-btn>
+        </template>
+        <span>Terms of Service</span>
+      </v-tooltip>
+    </v-app-bar>
+    <v-content>
+      <AppsList :apps="apps" />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import AppsList from "./components/AppsList";
+
+export default {
+  name: "App",
+  components: {
+    AppsList
+  },
+  data: () => ({
+    apps: require("./apps.json")
+  })
+};
+</script>
+
+<style scoped>
+.v-app-bar {
+  background: url("~@/assets/pattern.png");
+}
+
+.v-app-bar .v-btn {
+  height: 100% !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './components/App'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  vuetify,
   render: h => h(App)
-})
+}).$mount("#app");
