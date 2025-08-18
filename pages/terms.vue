d59241115197622c4fce4038031a23fac7f508e7
Tobias Rausch <rauschtobi@gmail.com>
2025-08-18 11:18:31 +0200

    use plain html

--- pages/terms.vue
+++ pages/terms.vue
@@ -1,20 +1,104 @@
 <template>
-  <v-sheet class="my-12 mx-auto px-12 py-10" max-width="800px" :elevation="2" v-html="terms"/>
+  <v-card max-width="900" class="mx-auto my-12 px-12 py-10" :elevation="2">
+    <v-list-item>
+      <v-icon left>fas fa-balance-scale</v-icon>
+    </v-list-item>
+    <v-card-text>
+      <h2>Terms of Use</h2>
+      <p>
+        The use and access of the GEAR website
+        (<a href="https://www.gear-genomics.com" target="_blank" rel="noopener">www.gear-genomics.com</a>) and its associated
+        Software (“the Site”) is subject to the following terms of use. Please read these terms carefully before accessing
+        or using the Site.
+      </p>
+      <h2>General Description</h2>
+      <p>
+        The Site is a web application server that hosts a variety of tools for analyzing genomic data. Data processing is carried
+        out in a fully automated fashion, i.e. no manual inspection or any other manual handling of the input data is performed.
+        For usage and description of an individual tool, please consult its dedicated documentation available in the
+        source code repository of GEAR (<a href="https://github.com/gear-genomics" target="_blank" rel="noopener">https://github.com/gear-genomics</a>).
+      </p>
+      <h2>Warranty Disclaimer/Limitation of Liability</h2>
+      <p>
+        The information and contents which are provided by EMBL through the Site (“the Contents”) are delivered to you "as is", without
+        any warranties of any kind, express or implied, including without limitation warranties as to the fitness for any
+        particular purpose, or non-infringement of third parties’ rights, whether or not discoverable.
+      </p>
+      <p>
+        Although EMBL is confident that the Site provides accurate results (EMBL uses the Site for own internal purposes), you hereby
+        understand and acknowledge that the Contents are based on your uploaded data and data provided by other third parties,
+        over which EMBL has no control at all and which may contain errors that EMBL cannot identify and which may affect
+        the Contents. Further, the provision of Contents, including without limitation of analysis and results thereof, as
+        well as any related services, is made by EMBL using software, algorithms and computer-implemented technologies (whether
+        owned by EMBL or by third parties) the proper functioning of which cannot be guaranteed. No warranty is provided
+        in this respect, in particular that such software, algorithms or technology would be error-free, flawless or exempt
+        from any bug or another unintentional malfunction. EMBL cannot be held responsible for any interpretation of the
+        results and other data in the Contents. The use of this service is also subject to the terms and conditions of the
+        original providers of reference data resources and third-party software libraries.
+      </p>
+      <h2>Data protection Disclaimer</h2>
+      <p>
+        It is your responsibility to ensure that the transfer of data to the Site and the processing of data by the Site is covered
+        by any necessary consents and in scope of any restrictions governing use and/or transfer of data imposed by the original
+        data provider and/or the controller of the data. Furthermore you must ensure that it is impossible to identify or
+        re-identify the original data provider´s identity from the submitted data or the Consents.
+      </p>
+      <h2>Use Restriction</h2>
+      <p>
+        It is your responsibility to ensure that the submitted data are not illegal or otherwise objectionable (including, without
+        limitation, any file or content that is copyrighted or subject to third party proprietary rights, including privacy
+        or personality rights, rights protecting personal data, or otherwise).
+      </p>
+      <p>
+        You agree that you will not interfere or attempt to interfere with the proper functioning of the Site (including, without
+        limitation, take any action which may impose an unreasonable or disproportionately large load on our servers or network
+        infrastructure), or bypass any measures which restrict access to the Site or use any technology to scrape, spider
+        or crawl the Site or harvest or manipulate data.
+      </p>
+      <p>
+        Furthermore you agree that you will not use the Site for any unlawful purpose or for the promotion of illegal activities,
+        or for any activity that may harm another person or group (including, without limitation, by posting or linking to
+        malicious content intended to damage or disrupt another user’s browser or computer).
+      </p>
+      <h2>Medical information Disclaimer – Research purpose only</h2>
+      <p>
+        <strong>The Site is for research purpose only.</strong>
+        The aim of EMBL making the Site publicly available is to give you, as a scientist, the opportunity to benefit from EMBL´s
+        achievements and to support the overall research. Therefore you agree that the Site is providing a service that is
+        intended only to assist users in reaching their own conclusions or making their own research work but not for medical
+        treatments. Users are solely responsible for their decisions based on use of the Contents.
+      </p>
+      <h2>Data retention</h2>
+      <p>
+        The submitted data and the Contents provided to you are saved on the server for up to a maximum of 24 hours.
+        After expiration of this period, the submitted information and the Contents are deleted.
+      </p>
+      <h2>Cookies and Privacy Disclaimer</h2>
+      <p>
+        Information provided to or gathered by the Site are controlled by EMBL.
+      </p>
+      <p>
+        EMBL gathers information about users solely to improve the usability, stability and usefulness of the Site, and not for any
+        commercial purposes. No use is made of users' contact information except when directly requested by them, e.g. to
+        sign up for a mailing list or activate a user account. Other information collected during the course of your usage
+        of the site, such as pages visited, the IP-address, the browser version, the date and time of access and the requested
+        reference genome, may be collected in server log files and used for statistical analysis of traffic patterns. In
+        no event will EMBL thereby acquire information on the individual identity of any visitor and trace the user´s personal
+        identity.
+      </p>
+      <h2>External Links</h2>
+      <p>
+        Some GEAR applications and websites may contain hyperlinks to websites owned and operated by third parties. These third party
+        websites have their own privacy policies, including cookies, and we urge you to review them. They will govern the
+        use of personal information you submit or that is collected by cookies when you visit these websites. We do not accept
+        any responsibility or liability for the privacy practices of such third party websites and your use of such websites
+        is at your own risk.
+      </p>
+      <h2>GEAR License</h2>
+      <p>
+        The GEAR software is free and open-source. License details can be found in the source code repositories on GitHub
+        (<a href="https://github.com/gear-genomics" target="_blank" rel="noopener">https://github.com/gear-genomics</a>).
+      </p>
+    </v-card-text>
+  </v-card>
 </template>
-
-<script>
-import termsMd from '@/terms-of-use/terms.md?raw'
-import MarkdownIt from 'markdown-it'
-
-export default {
-  computed: {
-    terms() {
-      const md = new MarkdownIt({
-        linkify: true,
-        html: true
-      })
-      return md.render(termsMd)
-     }
-  }
-}
-</script>
