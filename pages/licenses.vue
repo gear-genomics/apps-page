75d619297ff4f1f5c0f16ae725f767bffb0bea15
Andreas Untergasser <andreas@untergasser.de>
2025-12-29 16:33:27 +0100

    Updated license page

--- pages/licenses.vue
+++ pages/licenses.vue
@@ -12,11 +12,57 @@
       </p>
 
       <p>
-      Teal: <a href='https://github.com/gear-genomics/teal/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      Alfred: <a href='https://github.com/tobiasrausch/alfred/blob/main/LICENSE' target="_blank">BSD 3-Clause License</a>
+      </p>
+      <p>
+      bgen: <a href='https://github.com/gear-genomics/bgen/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Bistro: <a href='https://github.com/gear-genomics/bistro/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Halo: <a href='https://github.com/gear-genomics/halo/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Indigo: <a href='https://github.com/gear-genomics/indigo/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      maze: <a href='https://github.com/mhyfritz/maze/blob/master/LICENSE' target="_blank">MIT License</a>
+      </p>
+      <p>
+      Padlock: <a href='https://github.com/gear-genomics/padlock/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Pearl: <a href='https://github.com/gear-genomics/pearl/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Primer3Plus: <a href='https://github.com/primer3-org/primer3plus/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      RDML-Tools: <a href='https://github.com/RDML-consortium/rdml-tools/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      sabre: <a href='https://github.com/gear-genomics/sabre/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
       </p>
       <p>
       Sage: <a href='https://github.com/gear-genomics/sage/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
       </p>
+      <p>
+      Salt: <a href='https://github.com/gear-genomics/salt/blob/main/LICENSE.txt' target="_blank">MIT License</a>
+      </p>
+      <p>
+      Silica: <a href='https://github.com/gear-genomics/silica/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Teal: <a href='https://github.com/gear-genomics/teal/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Wally: <a href='https://github.com/gear-genomics/wallyapp/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      <p>
+      Wily DNA Editor: <a href='https://github.com/gear-genomics/wily-dna-editor/blob/main/LICENSE' target="_blank">GNU General Public License v3.0</a>
+      </p>
+      
     </v-card-text>
   </v-card>
 </template>
